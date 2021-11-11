ba7fa8db342e6dea3ac7af8892d3ba0591adfdba
alan <alandelleore@gmail.com>
2021-11-10 23:10:36 -0300

    segunda entrega react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
+    <div>
+      <NavBar/>
+    <div>
+
+    </div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ***Bluebird-shop***
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+    return (
+        <nav className="container">
+            <ul>
+                <li>Enlace1</li>
+                <li>Enlace2</li>
+                <li>Enlace3</li>
+                <li>Enlace4</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
