470f0604bf136f7b1e5d6db86ae3aea4731e83bd
alan <alandelleore@gmail.com>
2021-11-17 19:36:26 -0300

    Aclase 5 - Contador con boton

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import NavBar from "./components/Navbar/NavBar";
 import Header from './components/Header/Header';
 import ItemListContainer from './components/Itemlistcontainer/ItemListContainer';
-
+import ItemCount from './components/ItemCount/ItemCount';
 
 
 class App extends Component {
@@ -17,6 +17,10 @@ render() {
       <ItemListContainer
         greeting='Bienvenido a Bluebird, tienda de Fragancias..'
         info='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the  when an unknown printer took a galley of type and scrambled it to make a type specimen It has survived not only five centuries but also the leap into electronic typesettremaining essentially unchanged It was popularised in the with the release of Letraset sheets containing Lorem Ipsum passages and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem '
+        />
+        <ItemCount 
+        stock='20'
+        initial='0'
         />
     </>
   );
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import './ItemCount.css'
+
+const ItemCount = ({stock, initial}) => {
+   
+    const [counter, setCounter] = useState(parseInt(initial));
+    
+    const handleCounterUp = () => {
+      if (counter >= 0 && counter < stock) {
+      setCounter(counter + 1);
+    } 
+    };
+    const handleCounterDown = () => {
+      if (counter >= 1) {
+        setCounter(counter - 1);
+      } 
+    }
+  
+    return (
+      <div className="App">
+        <h1 className="stock">Stock : {stock} unid.</h1>
+        <p className="counter">{counter}</p>
+        <button  onClick={handleCounterDown}>-</button>
+        <button  onClick={handleCounterUp}>+</button>
+      </div>
+    );
+  };
+  
+  export default ItemCount;
